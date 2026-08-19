1046f91f3ecb26a9363566045f992c26fd7e84af
Abi Athmakuri <abi.launchcode@gmail.com>
2026-08-18 19:45:15 -0500

    added mock data (ref issue #2)

---
+++ price-watchdog/src/data/mockData.js
@@ -0,0 +1,28 @@
+const mockProducts = [
+    {
+        id: 1,
+        name: "Sony WH-1000XM5 Headphones",
+        currentPrice: 328.00,
+        targetPrice: 279.99,
+    },
+    {
+        id: 2,
+        name: "Instant Pot Duo 7-in-1",
+        currentPrice: 89.99,
+        targetPrice: 60.00,
+    },
+    {
+        id: 3,
+        name: "Kindle Paperwhite (11th Gen)",
+        currentPrice: 139.99,
+        targetPrice: 29.99,
+    },
+    {
+        id: 4,
+        name: "Anker 737 Power Bank",
+        currentPrice: 99.99,
+        targetPrice: null,
+    },
+];
+
+export default mockProducts;
