6059459cc711ec0cf2463ac8c6bc9e9dc8e87eb3
Abi Athmakuri <abi.launchcode@gmail.com>
2026-08-29 18:36:12 -0500

    added browser routing

--- price-watchdog/src/App.jsx
+++ price-watchdog/src/App.jsx
@@ -1,14 +1,19 @@
 import "./App.css";
-// import Dashboard from "./components/Dashboard";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home"
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
     return (
         <>
             <Navbar />
-            <Home />
-            {/* <Dashboard /> */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
         </>
     );
 }
--- price-watchdog/src/components/Navbar.jsx
+++ price-watchdog/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../styling/Navbar.css";
 
 export default function Navbar() {
@@ -5,9 +6,9 @@ export default function Navbar() {
         <>
             <h1 className="title">Price Watchdog</h1>
             <div className="links">
-                <h3>Home</h3>
-                <h3>Dashboard</h3>
-                <h3>About</h3>
+                <Link to="/">Home</Link>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/about">About</Link>
             </div>
         </>
     );
