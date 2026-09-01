666b3bc790b43b557ef33d20713acfc9ce18b04d
Abi Athmakuri <abi.launchcode@gmail.com>
2026-09-01 01:57:56 -0500

    footer added

--- price-watchdog/src/components/Footer.jsx
+++ price-watchdog/src/components/Footer.jsx
@@ -1,9 +1,11 @@
+import '../App.css'
+
 export default function Footer() {
     let currentYear = new Date().getFullYear();
 
     return (
         <footer>
-            <div>&copy; {currentYear} <a href="https://www.linkedin.com/in/abiathmakuri/">LinkedIn</a></div>
+            <div>&copy; {currentYear} Abi Athmakuri All Rights Reserved | <a href="https://www.linkedin.com/in/abiathmakuri/">LinkedIn</a></div>
         </footer>
     );
 }
